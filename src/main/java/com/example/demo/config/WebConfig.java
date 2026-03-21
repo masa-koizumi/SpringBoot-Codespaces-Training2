77716e3acdcc3f97b54103f33669af59f19340b3
masa-koizumi <m-koizumi@ems-n.jp>
2026-03-21 17:06:57 +0900

    Update WebConfig.java

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -2,7 +2,8 @@ package com.example.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -14,14 +15,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
 
         registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**") // 全て対象
                 .excludePathPatterns(
-                        "/",              // ログイン画面
-                        "/login",         // ログイン処理
-                        "/h2-console/**", // H2コンソール
+                        "/",          // ログイン画面
+                        "/login",     // ログイン処理
+                        "/h2-console/**", // H2
                         "/css/**",
-                        "/js/**",
-                        "/images/**",
-                        "/error"          // エラー画面
+                        "/js/**"
                 );
     }
 }

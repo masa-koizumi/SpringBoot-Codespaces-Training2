6816cfbdd5d28c0fc17458f9129053a37e21835e
masa-koizumi <m-koizumi@ems-n.jp>
2026-03-21 14:34:36 +0900

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,22 @@
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255)
+DROP TABLE IF EXISTS loans;
+DROP TABLE IF EXISTS assets;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+CREATE TABLE assets (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    status VARCHAR(20)
+);
+
+CREATE TABLE loans (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    asset_id BIGINT,
+    user_id BIGINT,
+    CONSTRAINT fk_asset FOREIGN KEY (asset_id) REFERENCES assets(id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
 );
